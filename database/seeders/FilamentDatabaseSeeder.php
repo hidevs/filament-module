05ba94fdab6d317b25dfb17881cc2b53ac814204
JalalLinuX <smjjalalzadeh93@gmail.com>
2026-01-03 14:02:49 +0330

    init module

---
+++ database/seeders/FilamentDatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Filament\Database\Seeders;
+
+use Modules\General\Contracts\Seeder\BaseSeeder;
+
+class FilamentDatabaseSeeder extends BaseSeeder
+{
+    public function init(): void
+    {
+        //
+    }
+
+    public function fake(): void
+    {
+        //
+    }
+}
